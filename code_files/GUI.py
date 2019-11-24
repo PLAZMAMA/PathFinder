6bd6eab7376b53b20588636d3db5569f5afebd13
PLAZMAMA <maorioriori@gmail.com>
2019-11-24 18:05:17 -0500

    added comments to the methods and wrote dysplay array method

--- code_files/GUI.py
+++ code_files/GUI.py
@@ -1,4 +1,6 @@
 from tkinter import *
+from PIL import Image, ImageTk
+import numpy as np
 
 class Window(Frame):
     def __init__(self, master, width = 800, height = 800):
@@ -9,27 +11,40 @@ class Window(Frame):
     
 
     def generate_maze(self):
-        pass
-    
-    def find_path(self):
+        """generates the maze to the canvas"""
         pass
 
+    
+    def find_path(self):
+        """finds the shortest path in the maze from the start to the end"""
+        pass
+
+    def display_array(self, array):
+        """displayes a given array on the canvas"""
+        img = ImageTk.PhotoImage(Image.fromarray(array))
+        self.canvas.create_image(0, 0, anchor = NW, image = img)
+        self.canvas.image = img
+
     def set_win(self):
+        """generates everything that is desplayed on the window and the functions of them"""
+
         #setting up the intial window
         self.master.title("Path Finder")
         self.master.geometry(f"{self.w}x{self.h}")
 
         #initializing the wigets
-        canvas = Canvas(self.master, width = self.w - int(self.w / 50), height = self.h - int(self.h / 4), bg = "blue")
+        self.canvas = Canvas(self.master, width = self.w - int(self.w / 50), height = self.h - int(self.h / 4), bg = "blue")
         generate_maze = Button(self.master, text = "Generate Maze", command = self.generate_maze)
         find_path = Button(self.master, text = "Find Path", command = self.find_path, height = int(self.h / 100), width = int(self.w / 50))
         
         #placing and updating widgets to get their dimentions which are used for later placement
         find_path.place(x = 0, y = 0)
         find_path.update()
+        self.canvas.place(x = 0, y = 0)
+        self.canvas.update()
 
         #placing the widgets
-        canvas.place(x = int(self.w/100), y = int(self.h / 5))
+        self.canvas.place(x = int(self.w/100), y = int(self.h / 5))
         generate_maze.place(x = int(self.w / 50) , y = int(self.h / 50))
         find_path.place(x = int(self.w / 2) - int(find_path.winfo_width() / 2), y = int(self.h / 50))
         
