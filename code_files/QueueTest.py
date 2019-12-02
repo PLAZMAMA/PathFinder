b88cb3e13171300b74ce5d08c961dd71158169b5
PLAZMAMA <maorioriori@gmail.com>
2019-12-02 17:18:59 -0500

    added comments to the test functions

--- code_files/QueueTest.py
+++ code_files/QueueTest.py
@@ -6,11 +6,11 @@ class QueueTest(TestCase):
         self.queue = Queue()
     
     def test_add(self):
-        """tests the add function"""
+        """tests the add function of the Queue class"""
         self.queue.add(1)
         self.assertEqual([1], self.queue.queue)
     
     def test_remove(self):
-        """tests the remove function"""
+        """tests the remove function of the Queue class"""
         a = self.queue.queue[0]
         self.assertEqual(a, self.queue.remove())
