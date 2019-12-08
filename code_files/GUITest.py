42969d4e07f162eaceff0d6c4a85407e24dd8bf2
PLAZMAMA <maorioriori@gmail.com>
2019-12-07 21:13:25 -0500

    removed test_create_grid method

--- code_files/GUITest.py
+++ code_files/GUITest.py
@@ -7,14 +7,6 @@ class GUITest(TestCase):
        self.root = Tk()
        self.app = Window(self.root)
        self.app.set_win()
-
-    def test_create_grid(self):
-        """tests the create grid method of the GUI class"""
-        expected = [[10, 206, 245], [0, 0, 0], [10, 206, 245], [0, 0, 0], [10, 206, 245], 
-                    [10, 206, 245], [0, 0, 0], [10, 206, 245], [0, 0, 0], [10, 206, 245],
-                    [10, 206, 245], [0, 0, 0], [10, 206, 245], [0, 0, 0], [10, 206, 245]]
-
-        self.assertEqual(expected, self.app.create_grid(6,6))
     
     def test_get_inpt_location(self):
         self.app.grid_size = [9,9]
